b847144ccbed3aff4ff0758347c5409c368bbaf0
Rishit605 <rishit.sisodia2001@gmail.com>
2024-09-16 16:26:07 +0530

    Updated the model architecture. Removed redundant code.

--- src/model/model.py
+++ src/model/model.py
@@ -46,18 +46,18 @@ class EarthquakeModel(nn.Module):
         super(EarthquakeModel, self).__init__()
         # self.input_size = input_size
         self.hidden_size = hidden_size
-        self.output_size = output_size
+        self.num_layers = num_layers
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True, dropout=dropout_prob, bidirectional=True)
         self.fc1 = nn.Linear(hidden_size * 2, hidden_size)
         self.relu = nn.ReLU()
         self.dropout = nn.Dropout(dropout_prob)
-        self.fc2 = nn.Linear(hidden_size * 2, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
         h0 = torch.zeros(self.num_layers * 2, x.size(0), self.hidden_size).to(x.device)
         c0 = torch.zeros(self.num_layers * 2, x.size(0), self.hidden_size).to(x.device)
         out, _ = self.lstm(x, (h0, c0))
-        out = self.fc1(out[:, 1, :])
+        out = self.fc1(out[:, -1, :])
         out = self.relu(out)
         out = self.dropout(out)
         out = self.fc2(out)
