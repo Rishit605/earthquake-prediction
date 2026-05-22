5afb54a0f5ae8da19e7769ca2c1b49c2ca08cc6f
Rishit605 <rishit.sisodia2001@gmail.com>
2026-05-22 20:58:00 +0530

    Update __init__.py

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -3,5 +3,5 @@ Model module containing the neural network architecture for earthquake predictio
 """
 
 from .model import EarthquakeModel, ModelCheckPoint, Early_Stopping
-
-__all__ = ['EarthquakeModel', 'ModelCheckPoint', 'Early_Stopping', ]
+from .lr_scratch import LinearR
+__all__ = ['EarthquakeModel', 'ModelCheckPoint', 'Early_Stopping', 'LinearR']
